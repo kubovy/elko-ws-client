b9752aff666a969ff4882f0fefc6c66a27117daf
Jan Kubovy <jan@kubovy.eu>
2018-04-07 17:36:37 +0000

    Simplyfy - no item config needed.

--- elko-ws-client.js
+++ elko-ws-client.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const child_process = require('child_process');
 const WebSocket = require('ws');
 const axios = require("axios");
 const fs = require('fs');
@@ -11,137 +12,24 @@ console.log("Starting...");
 let config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 if (DEBUG) console.log(config);
 
-function RGB2HSV(json) {
-	"use strict";
-	red = json.red;
-	green = json.green
-	blue = json.blue
-	brightness = json.brightness
-	var max = Math.max(red, green, blue),
-		min = Math.min(red, green, blue),
-		d = max - min,
-		hue,
-		saturation = (max === 0 ? 0 : d / max),
-		value = max / 255;
-
-	switch (max) {
-		case min: hue = 0; break;
-		case red: hue = (green - blue) + d * (green < blue ? 6: 0); hue /= 6 * d; break;
-		case green: hue = (blue - red) + d * 2; hue /= 6 * d; break;
-		case blue: hue = (red - green) + d * 4; hue /= 6 * d; break;
-	}
-
-	var hsv = {
-		hue: hue * 360,
-		saturation: saturation * 100,
-		value: brightness * 100 / 255 // was value
-	};
-	
-	return parseInt(hsv.hue) + ',' + parseInt(hsv.saturation) + ',' + parseInt(hsv.value);
-}
-
-/*
- * HTTP Server
- */
-function getUrl(deviceDefinition, which) {
-	"use strict";
-	let request = deviceDefinition[which];
-	return request.url.replace("{id}", deviceDefinition.id).replace("{item}", deviceDefinition.item);
-}
-
-function axiosRequest(deviceDefinition, which, data) {
-	"use strict";
-	let request = deviceDefinition[which];
-	let config = {
-		method: request.method.toLocaleLowerCase(),
-		url: getUrl(deviceDefinition, which),
-		headers: request.headers
-	};
-	if (data) config.data = data
-		if (DEBUG) console.log("Axios: " + JSON.stringify(config));
-	return axios.request(config);
-}
-
-function getResult(request, response) {
-	"use strict";
-	let result;
-	if (request.path) {
-			result = JSONPath(response.data, request.path);
-			if (DEBUG) console.log("Mapping: '" + JSON.stringify(response.data) + "' with '" + request.path + "' to '" + result + "'");
-		}
-	else result = response.data;
-	return result;
-}
-
-function logRequest(prefix, deviceDefinition, which, response) {
-	"use strict";
-	let request = deviceDefinition[which];
-	let result = getResult(request, response);
-	console.log(prefix, request.method + " " + getUrl(deviceDefinition, which), response.status + " " + response.statusText, result);
-}
-
-function update(deviceDefinition, state) {
-	"use strict";
-	axiosRequest(deviceDefinition, 'update', state).then(function(response) {
-		logRequest("Updated", deviceDefinition, 'update', response);
-	});
-}
-
-function check(deviceDefinition, state) {
-	"use strict";
-	let request = deviceDefinition.check
-	if (request) {
-		axiosRequest(deviceDefinition, 'check').then(function(response) {
-			let result = getResult(request, response)
-			logRequest("Checked:", deviceDefinition, 'check', response);
-			if (state != result) update(deviceDefinition, state);
-		});
-	} else {
-		update(deviceDefinition, state);
-	}
-}
-
-function process(deviceDefinition) {
-	"use strict";
-	axiosRequest(deviceDefinition, 'get').then(function(response) {
-		let request = deviceDefinition.get
-		let result = getResult(request, response);
-				if (DEBUG) console.log("Transfoming: let state=" + JSON.stringify(result) + ";" + deviceDefinition.transform + ';');
-				let state;
-				try {
-					state = eval("let state=" + JSON.stringify(result) + ";" + deviceDefinition.transform + ';');
-				} catch (e) {
-					console.log(e)
-				}
-				if (DEBUG) console.log("Transformation result: '" + state + "'");
-		logRequest("Processed:", deviceDefinition, 'get', response);
-		check(deviceDefinition, state);
-	});
-}
-
-var ws = new WebSocket(config['ws']);
+var subprocess = child_process.fork(`${__dirname}/elko-ws-process.js`);
+var ws = new WebSocket(config.ws);
 
 ws.on('open', function open() {
-	console.log('connected');
+	console.log('[WS] Client connected');
 });
 
 ws.on('close', function close() {
-	console.log('disconnected');
+	console.log('[WS] Client disconnected');
 });
 
 ws.on('message', function incoming(data) {
-	console.log("Incoming:", data);
+	console.log("[WS] Incoming:", data);
 	let pattern = RegExp(config['regex']);
 	let matches = pattern.exec(data);
 	if (matches.length > 1) {
 		let deviceId = matches[1];
-		let deviceDefinition = config.devices[deviceId];
-				if (deviceDefinition) {
-					deviceDefinition.id = deviceId;
-					process(deviceDefinition);
-				} else {
-					console.log("Device with the ID " + deviceId + " is not defined!");
-				}
+		subprocess.send(deviceId)
 	}
 });
 
