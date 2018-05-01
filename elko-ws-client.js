ac042107fedc49623f2fd79b07787310f4e97e10
Jan Kubovy <jan@kubovy.eu>
2018-05-01 13:45:45 +0200

    Update.

--- elko-ws-client.js
+++ elko-ws-client.js
@@ -2,9 +2,7 @@
 
 const child_process = require('child_process');
 const WebSocket = require('ws');
-const axios = require("axios");
 const fs = require('fs');
-const JSONPath = require('advanced-json-path');
 const DEBUG = false;
 
 console.log("Starting...");
@@ -12,25 +10,33 @@ console.log("Starting...");
 let config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 if (DEBUG) console.log(config);
 
-var subprocess = child_process.fork(`${__dirname}/elko-ws-process.js`);
-var ws = new WebSocket(config.ws);
+let subprocess = child_process.fork(`${__dirname}/elko-ws-process.js`);
+let webservices = [];
+for (let i in config['elko']) {
+	let host = config['elko'][i];
+	let ws = new WebSocket(config['ws'].replace("{{HOST}}", host));
 
-ws.on('open', function open() {
-	console.log('[WS] Client connected');
-});
+	ws.on('open', function open() {
+		console.log('[WS] Client connected');
+	});
 
-ws.on('close', function close() {
-	console.log('[WS] Client disconnected');
-});
+	ws.on('close', function close() {
+		console.log('[WS] Client disconnected');
+	});
 
-ws.on('message', function incoming(data) {
-	console.log("[WS] Incoming:", data);
-	let pattern = RegExp(config['regex']);
-	let matches = pattern.exec(data);
-	if (matches.length > 1) {
-		let deviceId = matches[1];
-		subprocess.send(deviceId)
-	}
-});
+	ws.on('message', function incoming(data) {
+		console.log("[WS] Incoming (" + host + "):", data);
+		let pattern = RegExp(config['regex']);
+		let matches = pattern.exec(data);
+		if (matches && matches.length > 1) {
+			let deviceId = matches[1];
+			subprocess.send({"deviceId": deviceId, "source": host})
+		}
+	});
+
+	webservices.push(ws);
+}
+
+
 
 
--- elko-ws-process.js
+++ elko-ws-process.js
@@ -1,9 +1,7 @@
 "use strict";
 
-const WebSocket = require('ws');
 const axios = require("axios");
 const fs = require('fs');
-const JSONPath = require('advanced-json-path');
 const mqtt = require('mqtt');
 
 const DEBUG = false;
@@ -12,16 +10,19 @@ console.log("Starting...");
 
 let config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 if (DEBUG) console.log(config);
-let client = mqtt.connect(config.broker)
+let client = mqtt.connect(config['broker']);
 
+/**
+ * @return {string}
+ */
 function RGB2HSV(red, green, blue, brightness) {
 	"use strict";
-	var max = Math.max(red, green, blue),
+	let max = Math.max(red, green, blue),
 		min = Math.min(red, green, blue),
 		d = max - min,
 		hue,
-		saturation = (max === 0 ? 0 : d / max),
-		value = max / 255;
+		saturation = (max === 0 ? 0 : d / max);
+		//value = max / 255;
 
 	switch (max) {
 		case min: hue = 0; break;
@@ -30,46 +31,61 @@ function RGB2HSV(red, green, blue, brightness) {
 		case blue: hue = (red - green) + d * 4; hue /= 6 * d; break;
 	}
 
-	var hsv = {
+	let hsv = {
 		hue: hue * 360,
 		saturation: saturation * 100,
 		value: brightness * 100 / 255 // was value
 	};
-	
+
 	return parseInt(hsv.hue) + ',' + parseInt(hsv.saturation) + ',' + parseInt(hsv.value);
 }
 
 /*
  * HTTP Server
  */
-function publishItemState(deviceId, state) {
+function publishItemState(topic, payload, defaultValue) {
 	"use strict";
-	console.log("[MQTT] Publish: elko/" + deviceId + ": " + state);
+	console.log("[MQTT] Publish: elko/state/" + topic + ": " + payload);
 	//client.subscribe('elko/control/#')
-	client.publish("elko/state/" + deviceId, state, { "qos": 1, "retain": true});
+	if (payload === null || payload === "null") payload = defaultValue;
+	client.publish("elko/state/" + topic, '' + payload, { "qos": 1, "retain": true});
 }
 
-function processDevice(deviceId) {
+function processDevice(deviceId, host) {
 	"use strict";
 
-	console.log("[HTTP] GET " + config.url + (config.prefix || '/') + deviceId + (config.suffix || ''));
+	let url = config['state'].replace("{{HOST}}", host).replace("{{DEVICE_ID}}", deviceId);
+	console.log("[HTTP] GET " + url);
 	axios.request({
 		method: 'GET',
-		url: config.url + (config.prefix || '/') + deviceId + (config.suffix || '')
+		url: url
 	}).then(function(response) {
-		let body = response.data
-		let state = body
-		console.log("[HTTP] Response: " + JSON.stringify(body) + ' - ' + body.hasOwnProperty('blue'));		
+		let body = response.data;
+		console.log("[HTTP] Response (" + host + "): " + JSON.stringify(body) + ' - ' + body.hasOwnProperty('blue'));
 
 		if (body.hasOwnProperty('red') && body.hasOwnProperty('green') && body.hasOwnProperty('blue') && body.hasOwnProperty('brightness')) {
-			state = RGB2HSV(body.red, body.green, body.blue, body.brightness);
+			let state = RGB2HSV(body['red'], body['green'], body['blue'], body['brightness']);
+			publishItemState(deviceId, state, "0,0,0");
 		} else if (body.hasOwnProperty('brightness')) {
-			state = body.brightness + '';
+			let state = body['brightness'] + '';
+			publishItemState(deviceId, state, "0");
 		} else if (body.hasOwnProperty('on')) {
-			state = body.on ? 'ON' : 'OFF';
+			let state = body['on'] ? 'ON' : 'OFF';
+			publishItemState(deviceId, state, "OFF");
+		} else {
+			if (body.hasOwnProperty('temperature')) publishItemState(deviceId + "/temperature", body['temperature'], "0");
+			if (body.hasOwnProperty('temperature IN')) publishItemState(deviceId + "/temperature-in", body['temperature IN'], "0");
+			if (body.hasOwnProperty('temperature OUT')) publishItemState(deviceId + "/temperature-out", body['temperature OUT'], "0");
+			if (body.hasOwnProperty('battery')) publishItemState(deviceId + "/battery", body['battery'] ? 'OPEN' : 'CLOSED', "CLOSED");
+			if (body.hasOwnProperty('open window')) publishItemState(deviceId + "/open-window", body['open window'] ? 'OPEN' : 'CLOSED', "CLOSED");
+			if (body.hasOwnProperty('open valve')) publishItemState(deviceId + "/open-valve", body['open valve'], "0");
+			if (body.hasOwnProperty('requested temperature')) publishItemState(deviceId + "/requested-temperature", body['requested temperature'], "0");
+			if (body.hasOwnProperty('open window sensitivity')) publishItemState(deviceId + "/open-window-sensitivity", body['open window sensitivity'], "0");
+			if (body.hasOwnProperty('open window off time')) publishItemState(deviceId + "/open-window-off-time", body['open window off time'], "0");
+			if (body.hasOwnProperty('locked')) publishItemState(deviceId + "/locked", body['locked'] ? 'OPEN' : 'CLOSED', "CLOSED");
+			if (body.hasOwnProperty('error')) publishItemState(deviceId + "/error", body['error'] ? 'OPEN' : 'CLOSED', "CLOSED");
 		}
-		if (DEBUG) console.log("Result: '" + JSON.stringify(state) + "'");
-		publishItemState(deviceId, state);
+		if (DEBUG) console.log("Result (" + host + "): '" + state + "'");
 	});
 }
 
@@ -79,9 +95,9 @@ client.on('connect', function() {
 
 client.on('message', function (topic, message) {
 	console.log("[MQTT] Message: " + topic + ": " + message.toString())
-})
+});
 
 process.on('message', function(msg) {
-	processDevice(msg)
+	processDevice(msg["deviceId"], msg["source"])
 });
 
